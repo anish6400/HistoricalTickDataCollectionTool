d1acbebfcff0de2dcd50e2c6c15fa9990e50e111
Anish <anishkumar6400@gmail.com>
2022-12-25 22:39:16 -0800

    implemented startDate and endDate calculations for a contract

--- Contract.cs
+++ Contract.cs
@@ -5,34 +5,50 @@ namespace NinjaTrader.Custom.AddOns.HistoricalTickDataCollectionTool
     public class Contract
     {
         private string instrumentName;
-        private int month;
-        private int year;
 
-        public Contract(string instrumentName, int month, int year)
+        // both startDate and endDate are inclusive
+        private int startMonth;
+        private int startYear;
+        private DateTime startDate;
+
+        private int endMonth;
+        private int endYear;
+        private DateTime endDate;
+
+        // Sets the startDate to the sunday after 3rd friday of startMonth
+        private void SetStartDate()
+        {
+            DateTime sampleStartDate = new DateTime(endYear, endMonth, 1).AddMonths(-3);
+
+            DateTime thirdFridayOfMonth = Utils.FindDay(sampleStartDate.Year, sampleStartDate.Month, DayOfWeek.Friday, 3);
+
+            startDate = thirdFridayOfMonth.AddDays(2);
+        }
+
+        private void SetEndDate()
+        {
+            endDate = Utils.FindDay(endYear, endMonth, DayOfWeek.Friday, 3);
+        }
+
+        public Contract(string instrumentName, int endMonth, int endYear)
         {
             this.instrumentName = instrumentName;
-            this.month = month;
-            this.year = year;
+            this.endMonth = endMonth;
+            this.endYear = endYear;
+
+            SetStartDate();
+            SetEndDate();
         }
 
         public string InstrumentName { get { return instrumentName; } }
-        public int Month { get { return month; } }
-        public int Year { get { return year; } }
-
-        public DateTime GetStartDate()
-        {
-            // TODO
-            return DateTime.Now;
-        }
-        public DateTime GetEndDate()
-        {
-            // TODO
-            return DateTime.Now;
-        }
+        public int EndMonth { get { return endMonth; } }
+        public int EndYear { get { return endYear; } }
+        public DateTime StartDate { get { return startDate; } }
+        public DateTime EndDate { get { return endDate; } }
 
         public override string ToString()
         {
-            return instrumentName + " " + month.ToString("D2") + "-" + (year % 100).ToString();
+            return instrumentName + " " + endMonth.ToString("D2") + "-" + (endYear % 100).ToString();
         }
     }
 }
--- Utils.cs
+++ Utils.cs
@@ -1,8 +1,29 @@
+using System;
+
 namespace NinjaTrader.Custom.AddOns.HistoricalTickDataCollectionTool
 {
     public static class Utils
     {
-        // contract name should be in this pattern: InstrumentName MM-YY 
-        
+
+        /*
+         Thanks to: https://stackoverflow.com/a/20691099/16867234
+         */
+        public static DateTime FindDay(int year, int month, DayOfWeek Day, int occurance)
+        {
+            if (occurance <= 0 || occurance > 5) throw new Exception("Occurance is invalid");
+
+            DateTime firstDayOfMonth = new DateTime(year, month, 1);
+            //Substract first day of the month with the required day of the week 
+            var daysneeded = (int)Day - (int)firstDayOfMonth.DayOfWeek;
+            //if it is less than zero we need to get the next week day (add 7 days)
+            if (daysneeded < 0) daysneeded = daysneeded + 7;
+            //DayOfWeek is zero index based; multiply by the Occurance to get the day
+            var resultedDay = (daysneeded + 1) + (7 * (occurance - 1));
+
+            if (resultedDay > (firstDayOfMonth.AddMonths(1) - firstDayOfMonth).Days)
+                throw new Exception(String.Format("No {0} occurance(s) of {1} in the required month", occurance, Day.ToString()));
+
+            return new DateTime(year, month, resultedDay);
+        }
     }
 }
